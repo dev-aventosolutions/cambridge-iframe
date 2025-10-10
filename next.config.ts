be288f25f3ac39a74c5293bf8140f0c1a5026311
Arham-Ramay <arhamramay80@gmail.com>
2025-10-10 14:58:43 +0500

    Add figma design

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,21 @@
-// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // ✅ Static export
+  output: 'export',
+
+  // ✅ Custom headers
   async headers() {
     return [
       {
-        source: "/(.*)", 
+        source: "/(.*)", // all routes
         headers: [
           {
             key: "Content-Security-Policy",
-            value: "frame-ancestors 'self' http://localhost:3000 https://alphalogisticspk.com", 
+            value: "frame-ancestors 'self' http://localhost:3000 https://alphalogisticspk.com",
           },
           {
             key: "X-Frame-Options",
-            value: "ALLOW-FROM http://localhost:3000", 
+            value: "ALLOW-FROM http://localhost:3000", // optional, older browsers
           },
         ],
       },
